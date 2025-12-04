edfff51e5d2ff660fa429f78c18d66a81bd8e979
Roman <ignatenko_roman.s@mail.ru>
2025-12-04 23:28:34 +0300

    feat: add connection to db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './db';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
