415fa71f0e7149c5a287a50f79ab8235eedbafcd
roman23323 <ignatenko_roman.s@mail.ru>
2025-12-05 21:56:40 +0300

    feat: add DB model for uploaded file

---
+++ backend/src/models/Upload.ts
@@ -0,0 +1,21 @@
+import { Date, Document, model, Schema } from "mongoose";
+
+interface IUpload extends Document {
+    userId: string;
+    originalName: string;
+    storageKey: string;
+    size: number;
+    uploadedAt: Date;
+    mimeType: string;
+}
+
+const uploadSchema = new Schema<IUpload>({
+  userId: { type: String, required: true },
+  originalName: { type: String, required: true },
+  storageKey: { type: String, required: true },
+  size: { type: Number, required: true },
+  uploadedAt: { type: Date, default: Date.now },
+  mimeType: { type: String, required: true }
+});
+
+export const Upload = model<IUpload>('Upload', uploadSchema);
